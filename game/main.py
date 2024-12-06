8bc80b88c7921a87c137b878ef176f283963da02
RavenJhon2002 <ravenbuhay25@gmail.com>
2024-12-07 02:56:35 +0800

    update healthbar

--- game/main.py
+++ game/main.py
@@ -134,3 +134,11 @@ class Player(wizard):
                         self.score+=10
                         if ball in self.balls:
                             self.balls.remove(ball)
+   
+    def draw(self, window):
+        super().draw(window)
+        self.healthbar(window)
+
+    def healthbar(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.wizard_img.get_height() + 10, self.wizard_img.get_width(), 10))
+        pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.wizard_img.get_height() + 10, self.wizard_img.get_width() * (self.health/self.max_health), 10))
