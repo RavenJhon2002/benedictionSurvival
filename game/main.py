62711b5e56956844948d0ef1d2dbf56d0b646e7c
Villavicencio18 <Villavicencio.252135@balayan.sti.edu.ph>
2024-12-07 02:22:55 +0800

    added a code for the ball

--- game/main.py
+++ game/main.py
@@ -38,3 +38,24 @@ FIREBALL = pygame.image.load(os.path.join("assets", "fireball.png"))
 
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "bg1.png")), (WIDTH, HEIGHT))
+
+# code sa ball
+class Ball:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return not(self.y <= height and self.y >= 0)
+
+    def collision(self, obj):
+        return collide(self, obj)
+
